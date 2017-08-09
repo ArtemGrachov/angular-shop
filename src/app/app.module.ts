370c435fee4c6ca346625646ee7ee838d1019de8
Artem Grachov <grachov2195@gmail.com>
2017-08-09 14:37:43 +0300

    Prepearing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AdminModule } from './admin/admin.module';
+import { NewsModule } from './news/news.module';
+import { ProductsModule } from './products/products.module';
+import { ProvidersModule } from './providers/providers.module';
+import { UsersModule } from './users/users.module';
+
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from '../app-routing.module';
+
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+    declarations: [HomeComponent, HeaderComponent],
+    imports: [AppRoutingModule],
+    exports: [HeaderComponent, AppRoutingModule],
+    providers: [],
+})
+export class CoreModule { }
