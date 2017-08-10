e757afadc396cff7925e2cf805352024c63d09eb
Artem Grachov <grachov2195@gmail.com>
2017-08-10 18:00:01 +0300

    Product, providers edit

--- src/app/products/products-edit/products-edit.component.ts
+++ src/app/products/products-edit/products-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { ProductsService } from '../products.service';
+import { ProvidersService } from '../../providers/providers.service';
+
+import { Product } from '../../shared/models/product.model';
+import { Provider } from '../../shared/models/provider.model';
 
 @Component({
   selector: 'app-products-edit',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-edit.component.css']
 })
 export class ProductsEditComponent implements OnInit {
+  public product: Product;
+  public productId: String;
+  public providersList: Provider[];
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public productsService: ProductsService,
+    public providersService: ProvidersService
+  ) { }
+
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.productId = params['id'];
+        this.product = this.productsService.getProduct(this.productId);
+      }
+    );
+    this.providersList = this.providersService.getProviders();
   }
-
 }
