71cfd6099db2cbcf5a60feeaffd5915a5307579e
Artem Grachov <grachov2195@gmail.com>
2017-08-11 10:35:31 +0300

    Dashboard orders page HTML

--- src/app/dash/dash-orders/dash-orders.component.ts
+++ src/app/dash/dash-orders/dash-orders.component.ts
@@ -10,7 +10,7 @@ import { Order } from '../../shared/models/order.model';
 export class DashOrdersComponent implements OnInit {
   orders: Order[] = [
     new Order('1', '0', [{ name: 'Apple', price: 5.99 }], new Date(2017, 4, 4, 15, 12, 12, 100)),
-    new Order('1', '0', [{ name: 'Coca-Cola', price: 7.99 }], new Date(2017, 12, 3, 10, 5, 5, 0)),
+    new Order('1', '0', [{ name: 'Coca-Cola', price: 7.99 }, { name: 'Ice Cream', price: 2.99 }], new Date(2017, 0, 3, 10, 5, 5, 0)),
   ];
 
   constructor() { }
