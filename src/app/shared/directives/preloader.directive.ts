d52b43a9c866843a32148f86fa889c8145e97ad3
Artem Grachov <grachov2195@gmail.com>
2017-08-30 11:40:00 +0300

    Spinner

--- src/app/shared/directives/preloader.directive.ts
+++ src/app/shared/directives/preloader.directive.ts
@@ -5,6 +5,67 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class PreloaderDirective {
     constructor(private elRef: ElementRef) {
-        this.elRef.nativeElement.innerHTML = '<h1>Wait a moment</h1>';
+        this.spinner = {
+            radius: 300,
+            center: 150,
+            speed: 1,
+            duration: 1,
+            size: 10,
+            color: 'black'
+        };
+        this.elRef.nativeElement.innerHTML = `
+            <svg
+                width="${this.spinner.radius}"
+                height="${this.spinner.radius}">
+            <circle
+                id="circle1"
+                cx="${this.spinner.center}"
+                cy="${this.spinner.size}"
+                r="${this.spinner.size}"
+                fill="${this.spinner.color}" />
+            <circle
+                id="circle2"
+                cx="${this.spinner.center}"
+                cy="${this.spinner.size}"
+                r="${this.spinner.size * 0.8}"
+                fill="${this.spinner.color}" />
+            <circle
+                id="circle3"
+                cx="${this.spinner.center}"
+                cy="${this.spinner.size}"
+                r="${this.spinner.size * 0.5}"
+                fill="${this.spinner.color}" />
+            <animateTransform
+                xlink:href="#circle1"
+                attributeName="transform"
+                type="rotate"
+                from="0 ${this.spinner.center} ${this.spinner.center}"
+                to="360 ${this.spinner.center} ${this.spinner.center}"
+                attributeType="XML"
+                dur="${this.spinner.speed}s"
+                id="rotate1"
+                begin="0;rotate1.end + ${this.spinner.speed * 0.8}s" />
+            <animateTransform
+                xlink:href="#circle2"
+                attributeName="transform"
+                type="rotate"
+                from="0 ${this.spinner.center} ${this.spinner.center}"
+                to="360 ${this.spinner.center} ${this.spinner.center}"
+                attributeType="XML"
+                dur="${this.spinner.speed * 0.8}s"
+                id="rotate2"
+                begin="rotate1.begin + ${this.spinner.speed}s" />
+            <animateTransform
+                xlink:href="#circle3"
+                attributeName="transform"
+                type="rotate"
+                from="0 ${this.spinner.center} ${this.spinner.center}"
+                to="360 ${this.spinner.center} ${this.spinner.center}"
+                attributeType="XML"
+                dur="${this.spinner.speed * 0.5}s"
+                id="rotate3"
+                begin="rotate1.begin + ${this.spinner.speed * 1.3}s" />
+        </svg>`;
     }
+    spinner: any;
 }
